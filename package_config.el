b6923fec93949682da6f4294cc79407f83db3e69
Hari Amoor <amoor.hari@gmail.com>
2020-03-05 01:02:06 -0500

    use quelpa in use-package by default

--- package_config.el
+++ package_config.el
@@ -15,7 +15,9 @@
   (require 'use-package)
   (setq use-package-always-ensure t))
 
-(use-package quelpa-use-package)
+(use-package quelpa-use-package
+  :config
+  (setq use-package-ensure-function 'quelpa))
 
 (use-package dired+
   :quelpa (dired+ :fetcher github :repo "emacsmirror/dired-plus"))
