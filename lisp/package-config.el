06aedbdcd1d67fb8435658d04212ae8d1fcbdb6d
Hari Amoor <amoor.hari@gmail.com>
2020-03-15 00:40:53 -0400

    added thing

--- lisp/package-config.el
+++ lisp/package-config.el
@@ -22,7 +22,8 @@
   :config
   (autoload 'dired-async-mode "dired-async.el" nil t)
   (dired-async-mode 1)
-  (async-bytecomp-package-mode '(all)))
+  (async-bytecomp-package-mode 1)
+  (async-bytecomp-allowed-packages '(all)))
 
 (use-package diminish)
 
