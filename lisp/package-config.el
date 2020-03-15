16f2007769ba3bee65602872a4d4e459381afad7
Hari Amoor <amoor.hari@gmail.com>
2020-03-15 00:16:24 -0400

    wrong name

--- lisp/package-config.el
+++ lisp/package-config.el
@@ -18,7 +18,7 @@
 (straight-use-package 'use-package)
 (setq straight-use-package-by-default t)
 
-(use-package emacs-async
+(use-package async 
   :config
   (autoload 'dired-async-mode "dired-async.el" nil t)
   (dired-async-mode 1)
