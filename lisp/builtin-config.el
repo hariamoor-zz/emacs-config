d7dc412ddef4762077fd32e4725eba357ba46ce3
Hari Amoor <amoor.hari@gmail.com>
2020-03-21 21:48:24 -0400

    forgot to add builtin-config

---
+++ lisp/builtin-config.el
@@ -0,0 +1,18 @@
+;; configurations of builtin emacs systems
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory))
+      auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t))
+      custom-safe-themes t)
+
+;; needed to make tramp work, see:
+;; emacs.stackexchange.com/questions/12487/when-should-i-use-with-eval-after-load-in-my-configuration-files
+(with-eval-after-load 'tramp
+  (setq tramp-shell-prompt-pattern
+	"\\(?:^\\|\r\\)[^]#$%>\n]*#?[]#$%>].* *\\(^[\\[[0-9;]*[a-zA-Z] *\\)*"))
+
+(provide 'builtin-config)
